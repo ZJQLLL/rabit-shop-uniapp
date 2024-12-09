4ec96ac5ee20f969473e513b0656fa49d3b1a86e
zjq <2513667729@qq.com>
2024-12-09 11:18:30 +0800

    请求函数封装

--- src/pages/my/my.vue
+++ src/pages/my/my.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
 import { useMemberStore } from '@/stores'
-import '@/utils/http'
+import {http} from '@/utils/http'
 const memberStore = useMemberStore()
-const getData = () => {
-  uni.request({
-    method: 'GET',
-    url: '/home/banner',
+const getData =async () => {
+ const res = await http({
+    method:'GET',
+    url:'/home/banner',
   })
+  
 }
 </script>
 
--- src/utils/http.ts
+++ src/utils/http.ts
@@ -29,3 +29,46 @@ const httpInterceptor = {
 
 uni.addInterceptor('request', httpInterceptor)
 uni.addInterceptor('uploadFile', httpInterceptor)
+
+
+interface Data<T> {
+    code:string,
+    msg:string,
+    result?:T,
+}
+
+//添加类型,支持泛型
+export const http =<T> (options:UniApp.RequestOptions)=>{
+    //1、返回promise对象
+    return new Promise<Data<T>>((resolve,reject)=>{
+        uni.request({
+           ...options,
+           //2、请求成功
+           success(result) {
+            if(result.statusCode>=200&&result.statusCode<300){
+                 //2.1 提取核心数据
+               resolve(result.data as Data<T>)
+            }else if(result.statusCode===401){
+                //token信息失效,清理token信息,跳转登录页
+                const memberStore = useMemberStore()
+                memberStore.clearProfile()
+                uni.navigateTo({url:'pages/login/login'})
+                reject(result)
+            }else {
+                uni.showToast({
+                    icon:'none',
+                    title:(result.data as Data<T>).msg ?? '请求错误'
+                })
+            }    
+           },
+           //3、响应失败
+           fail(err) {
+               uni.showToast({
+                icon:'none',
+                title:'网络异常'
+               })
+               reject(err)
+           },
+        })
+    })
+}
