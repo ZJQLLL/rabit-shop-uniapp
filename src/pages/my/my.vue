0df2a8bd67d18ed3bde80de107879d72b1919d49
zjq <2513667729@qq.com>
2024-12-12 14:09:06 +0800

    退出登录

--- src/pages/my/my.vue
+++ src/pages/my/my.vue
@@ -22,7 +22,7 @@ const {guessRef,onScrolltolower} = useGuessLikeList()
     <!-- 个人资料 -->
     <view class="profile" :style="{ paddingTop: safeAreaInsets!.top + 'px' }">
       <!-- 情况1：已登录 -->
-      <view class="overview" v-if="true">
+      <view class="overview" v-if="memberStore.profile">
         <navigator url="/pagesMember/profile/profile" hover-class="none">
           <image
             class="avatar"
