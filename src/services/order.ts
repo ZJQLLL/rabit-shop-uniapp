a1ec5c8dcc101eb1e34d4e80c134f31d8d368cfe
zjq <2513667729@qq.com>
2024-12-24 16:09:15 +0800

    订单详情-自定义导航栏动画交互+订单状态渲染

--- src/services/order.ts
+++ src/services/order.ts
@@ -1,5 +1,29 @@
 import { http } from "@/utils/http";
 
+export enum OrderState {
+    /** 待付款 */
+  DaiFuKuan = 1,
+  /** 待发货 */
+  DaiFaHuo = 2,
+  /** 待收货 */
+  DaiShouHuo = 3,
+  /** 待评价 */
+  DaiPingJia = 4,
+  /** 已完成 */
+  YiWanCheng = 5,
+  /** 已取消 */
+  YiQuXiao = 6,
+}
+/** 订单状态列表 */
+export const orderStateList = [
+    { id: 0, text: '' },
+    { id: 1, text: '待付款' },
+    { id: 2, text: '待发货' },
+    { id: 3, text: '待收货' },
+    { id: 4, text: '待评价' },
+    { id: 5, text: '已完成' },
+    { id: 6, text: '已取消' },
+  ]
 export interface GetMemberOrderPreResult {
     /**
      * 商品集合
@@ -171,6 +195,156 @@ export interface SubmitOrderParams{
      */
     payType: number;
 }
+export interface GetMemberOrderDetailResult {
+    /**
+     * 预计到货时间
+     */
+    arrivalEstimatedTime: string;
+    /**
+     * 交易关闭时间
+     */
+    closeTime: string;
+    /**
+     * 发货时间
+     */
+    consignTime: string;
+    /**
+     * 倒计时--剩余的秒数 -1 表示已经超时，正数表示倒计时未结束
+     */
+    countdown: number;
+    /**
+     * 下单时间
+     */
+    createTime: string;
+    /**
+     * 配送时间类型，1为不限，2为工作日，3为双休或假日
+     */
+    deliveryTimeType: number;
+    /**
+     * 交易完成时间
+     */
+    endTime: string;
+    /**
+     * 完成评价时间
+     */
+    evaluationTime: string;
+    /**
+     * 订单编号
+     */
+    id: string;
+    /**
+     * 订单状态，1为待付款、2为待发货、3为待收货、4为待评价、5为已完成、6为已取消
+     */
+    orderState: number;
+    /**
+     * 支付渠道，1支付宝、2微信
+     */
+    payChannel: number;
+    /**
+     * 付款截止时间：剩余的秒数，前台转换成分钟：秒数
+     */
+    payLatestTime: number;
+    /**
+     * 实付金额
+     */
+    payMoney: string;
+    /**
+     * 付款时间
+     */
+    payTime: string;
+    /**
+     * 支付方式，1为在线支付，2为货到付款
+     */
+    payType: number;
+    /**
+     * 邮费
+     */
+    postFee: string;
+    /**
+     * 收货人地址
+     */
+    receiverAddress: string;
+    /**
+     * 收货人
+     */
+    receiverContact: string;
+    /**
+     * 收货人手机
+     */
+    receiverMobile: string;
+    /**
+     * 商品集合
+     */
+    skus: Skus[];
+    /**
+     * 金额合计
+     */
+    totalMoney: string;
+    /**
+     * 数量合计
+     */
+    totalNum: string;
+}
+/**
+ * 商品信息
+ */
+export interface Skus {
+    /**
+     * 属性说明
+     */
+    attrsText: string;
+    /**
+     * 单价
+     */
+    curPrice: number;
+    /**
+     * sku id
+     */
+    id: string;
+    /**
+     * 图片地址
+     */
+    image: string;
+    /**
+     * 商品名称
+     */
+    name: string;
+    /**
+     * 属性集合
+     */
+    properties: Property[];
+    /**
+     * 数量
+     */
+    quantity: string;
+    /**
+     * 实付金额
+     */
+    realPay: number;
+    /**
+     * spu id
+     */
+    spuId: string;
+    /**
+     * 小计
+     */
+    totalMoney: number;
+}
+
+/**
+ * 属性信息
+ */
+export interface Property {
+    /**
+     * 属性名称，如 颜色
+     */
+    propertyMainName: string;
+    /**
+     * 属性值名称，如 黑色
+     */
+    propertyValueName: string;
+}
+
 
 /**
  * 填写订单-获取预付订单
@@ -199,5 +373,34 @@ export const submitOrderAPI = (data:SubmitOrderParams)=>{
         method:'POST',
         url:'/member/order',
         data,
+    })
+}
+/**
+ * 获取订单详情
+ */
+export const getMemberOrderDetailAPI = (id:string)=>{
+    return http<GetMemberOrderDetailResult>({
+        method:'GET',
+        url:`/member/order/${id}`
+    })
+}
+/**
+ * 删除订单
+ */
+export const deleteMemberOrderAPI = (ids:string[])=>{
+    return http({
+        method:'DELETE',
+        url:'/member/order',
+        data:ids,
+    })
+}
+/**
+ * 取消订单
+ */
+export const cancelMemberOrderAPI = (id:string,cancelReason:string)=>{
+    return http<GetMemberOrderDetailResult>({
+        method:'PUT',
+        url:`/member/order/${id}/cancel`,
+        data:cancelReason,
     })
 }
